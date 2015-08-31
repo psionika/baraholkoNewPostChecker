d54fe735001400a3ac26e8f69d6e3795c6a6434f
psionika <psionika@bk.ru>
2015-08-31 19:19:59 +0800

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.ServiceModel.Syndication;
+using System.Net;
+using System.IO;
+using System.Globalization;
+using System.Xml.Linq;
+using System.Collections.ObjectModel;
+
+namespace baraholkoNewPostChecker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public ObservableCollection<RssFeed> RssFeedList { get; set; }        
+
+        public class RssFeed
+        {
+            public string CategoryTitle { get; set; }
+            public string Author { get; set; }
+            public string Category { get; set; }
+            public string Text { get; set; }
+            public string Date { get; set; }
+            public string Link { get; set; }
+        }
+
+        public DateTime PostLastDT = DateTime.Now;
+
+        void getBaraholkoRSS()
+        {
+            var uri = "http://baraholko.ru/rss.php";
+
+            var request = WebRequest.Create(uri);
+
+            using (WebResponse response = request.GetResponse())
+            using (XmlReader reader = XmlReader.Create(response.GetResponseStream()))
+            {
+                var feed = SyndicationFeed.Load(reader);
+
+                RssFeedList = new ObservableCollection<RssFeed>();
+
+                if (feed != null)
+                {
+                    foreach (var item in feed.Items)
+                    {
+                        if (item.Title.Text.ToString() == "Статистика конференции") continue;
+                        var rf = new RssFeed();
+
+                        rf.CategoryTitle = (item.Title != null) ? item.Title.Text : String.Empty;
+
+                        rf.Link = item.Links[0].Uri.ToString();
+
+                        if (item.Categories.Count != 0)
+                            rf.Category = item.Categories[0].Name;
+
+                        rf.Date = item.PublishDate.ToString();
+
+                        StringBuilder sb = new StringBuilder();
+                        foreach (SyndicationElementExtension extension in item.ElementExtensions)
+                        {
+                            XElement ele = extension.GetObject<XElement>();
+                            if (ele.Name.LocalName == "encoded" && ele.Name.Namespace.ToString().Contains("content"))
+                            {
+                                sb.Append(ele.Value + "<br/>");
+                            }
+                        }
+
+                        rf.Text = sb.ToString();
+
+                        if (item.Authors != null && item.Authors.Any())
+                        {
+                            rf.Author = item.Authors.First().Email;
+                        }
+
+
+                        RssFeedList.Add(rf);
+                    }
+                }
+            }
+
+            dataGridView1.DataSource = RssFeedList;
+        }
+
+        void button1_Click(object sender, EventArgs e)
+        {
+            getBaraholkoRSS();
+            
+            PostLastDT = DateTime.Parse(dataGridView1.Rows[0].Cells[4].Value.ToString());
+
+            label1.Text = PostLastDT.ToString();            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PostLastDT = DateTime.Now;
+        }
+    }
+
+ 
+}
